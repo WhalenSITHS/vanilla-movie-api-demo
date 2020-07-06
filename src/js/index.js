69d60b1283e5666fbb7fbf7904b998085ee0a1f1
Mike Whalen <Michael.whalen@sitechhs.com>
2020-07-06 08:25:40 -0400

    query

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 const key = "78117810";
-const query = async function(){
-    const response = await fetch(http://www.omdbapi.com/?s="Star Wars"&apikey=78117810)
-}
+const query = async function () {
+  const response = await fetch(
+    `http://www.omdbapi.com/?s="Star Wars"&apikey=${key}`
+  );
+};
