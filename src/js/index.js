fda8de2de797009040e50c604093ab269922e635
Mike Whalen <Michael.whalen@sitechhs.com>
2020-07-06 08:50:37 -0400

    PC crashed being safe

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const key = "78117810";
+const key = "1fd276ec57b4baedacae00246e5cf4b7";
 const query = async function () {
   const response = await fetch(
     `http://www.omdbapi.com/?s="Star Wars"&apikey=${key}`
